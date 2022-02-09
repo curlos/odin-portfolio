c37b80a2247d1141c687ae214307149eae380b4a
Carlos Martinez <curlosmart@gmail.com>
2022-02-09 17:02:42 -0500

    setup files and assets

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import AboutMe from './components/AboutMe'
+import Projects from './components/Projects'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <AboutMe />
+      <Projects />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
